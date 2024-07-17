23807be274f3f4a846dc1b21b5216c4039352240
raosahab14127 <eb20103109.raohammadali@gmail.com>
2024-07-17 05:42:19 -0700

    done

--- leads/models.py
+++ leads/models.py
@@ -1,14 +1,23 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    is_organisor = models.BooleanField(default=True)
-    is_agent = models.BooleanField(default=False)
+    pass
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
 
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    def __str__(self) -> str:
-        return f"{self.user.username}"
+    organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.email
 class Lead(models.Model):
     data  = (
         ('internet' , 'internet'),
@@ -25,3 +34,10 @@ class Lead(models.Model):
     agent = models.ForeignKey('Agent', on_delete=models.CASCADE)
     def __str__(self) -> str:
         return f"{self.firstname} {self.lastname}"
+    
+def post_user_created_signal(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+post_save.connect(post_user_created_signal, sender=User)
